4ef51456adf1a807f3d9ea449c8c3925e59727d1
Marcelo Alvarez <32470484+marcelo-alvarez@users.noreply.github.com>
2024-01-19 14:22:59 -0800

    added camb to conda packages

--- conf/conda-pkgs.sh
+++ conf/conda-pkgs.sh
@@ -9,6 +9,7 @@ conda config --set solver libmamba
 conda config --set channel_priority flexible
 conda install --yes -c conda-forge -c anaconda -c nvidia -c defaults \
     astropy \
+    camb \
     cuda-nvcc \
     cudatoolkit \
     fitsio \
